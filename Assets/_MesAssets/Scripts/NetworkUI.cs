474c7c8f666401f5552af130d1acd0ea144e0832
LaCoors <dave.lacourse@cegeptr.qc.ca>
2023-10-27 21:48:05 -0400

    peaufiné chap1

--- Assets/_MesAssets/Scripts/NetworkUI.cs
+++ Assets/_MesAssets/Scripts/NetworkUI.cs
@@ -10,16 +10,28 @@ public class NetworkUI : MonoBehaviour
     [SerializeField] private Button hostButton = default;
     [SerializeField] private Button serverButton = default;
     [SerializeField] private Button clientButton = default;
+    [SerializeField] private GameObject _canvasUIMenu = default;
+    [SerializeField] private GameObject _mainGaucheXRModel = default;
+    [SerializeField] private GameObject _mainDroiteXRModel = default;
 
     private void Start()
     {
         // souscrit à l'évènement sur le click du bouton Host et démarrer la session Host
         hostButton.onClick.AddListener(() =>NetworkManager.Singleton.StartHost());
+        hostButton.onClick.AddListener(() => FermerUI());
 
         // souscrit à l'évènement sur le click du bouton Host et démarrer la session Host
         serverButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
 
         // souscrit à l'évènement sur le click du bouton Host et démarrer la session Host
         clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+        clientButton.onClick.AddListener(() => FermerUI());
+    }
+
+    public void FermerUI()
+    {
+        _canvasUIMenu.SetActive(false);
+        _mainDroiteXRModel.SetActive(false);
+        _mainGaucheXRModel.SetActive(false);
     }
 }
